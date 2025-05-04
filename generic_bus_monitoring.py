366d5d1a38d7f1f9f9377477067fa47b2ff6dedf
Clement Lacau <lacau@insa-toulouse.fr>
2025-05-04 18:12:14 +0200

    add: double detection image

--- generic_bus_monitoring.py
+++ generic_bus_monitoring.py
@@ -46,7 +46,7 @@ class GenericBusMonitor(Module):
             ),
 
             # If read_counter too big -> ALERT
-            If(self.read_counter >= 1023, # dangerous reading size threshold
+            If(self.read_counter >= 4, # dangerous reading size threshold
                 self.alert.eq(1)
             ).Else(
                 If(~arbiter_bus.cyc & self.last_cyc,
@@ -171,7 +171,7 @@ class UARTSpy(Module):
                 self.data.eq(self.bus.dat_r),
                 self.ready.eq(1),
                 NextValue(self.addr, self.addr + 4),
-                If(self.addr + 4 >= 0x20001000,
+                If(self.addr + 4 >= 0x20000014,
                     NextState("WAIT_BEFORE_RESCAN") # we cycle to sleep for a while before rescanning
                 ).Else(
                     NextState("READ_KEY")
@@ -187,7 +187,88 @@ class UARTSpy(Module):
             NextValue(self.bus.cyc, 0),
             NextValue(self.bus.we, 0),
             NextValue(self.debug_read_enable, 0),
-            If(self.time_counter >= (5000),  # about ~10s
+            If(self.time_counter >= (20),  # about ~10s
+                NextValue(self.time_counter, 0),
+                NextValue(self.addr, 0x20000000),  # Reset address
+                NextState("BECOME_MASTER")  # Restart reading
+            ).Else(
+                NextValue(self.time_counter, self.time_counter + 1)
+            )
+        )
+
+        self.fsm.act("DONE",
+            self.ready.eq(1),
+            self.bus.cyc.eq(0),
+            self.bus.stb.eq(0),
+            self.bus.we.eq(0),
+        )
+
+
+
+# ----------- UART Spy (lit depuis RAM) -----------
+class RandomComponent(Module):
+    def __init__(self, bus):
+        self.bus = bus  # Wishbone master interface
+        self.ready = Signal(reset=0)
+        self.addr = Signal(32)
+        self.index = Signal(10)  # 10 bits to count up to 1024 words
+        self.data = Signal(32)
+        self.debug_read_data = Signal(32)
+        self.debug_read_enable = Signal()
+        self.uart_master_status = Signal()  # Status signal for UART becoming master
+        self.uart_slave_status = Signal()   # Status signal for UART becoming slave
+
+        self.submodules.fsm = FSM(reset_state="IDLE")
+
+        # Variables de temporisation
+        self.time_counter = Signal(32)
+
+        # Machine à états
+        self.fsm.act("IDLE",
+            NextValue(self.time_counter, 0),
+            If(self.time_counter == 20,
+                NextState("BECOME_MASTER")
+            ).Else(
+                NextValue(self.time_counter, self.time_counter + 1)
+            )
+        )
+
+        self.fsm.act("BECOME_MASTER",
+            NextValue(self.addr, 0x20000000),
+            NextValue(self.index, 0),
+            NextValue(self.uart_master_status, 1),
+            NextState("READ_KEY")
+        )
+
+        self.fsm.act("READ_KEY",
+            self.bus.adr.eq(self.addr[2:]),
+            self.bus.stb.eq(1),
+            self.bus.cyc.eq(1),
+            self.bus.we.eq(0),
+            self.debug_read_data.eq(self.data),
+            self.debug_read_enable.eq(0),
+            If(self.bus.ack,
+                self.debug_read_enable.eq(1),
+                self.data.eq(self.bus.dat_r),
+                self.ready.eq(1),
+                NextValue(self.addr, self.addr + 4),
+                If(self.addr + 4 >= 0x20000014,
+                    NextState("WAIT_BEFORE_RESCAN") # we cycle to sleep for a while before rescanning
+                ).Else(
+                    NextState("READ_KEY")
+                )
+
+            )
+        )
+
+        self.fsm.act("WAIT_BEFORE_RESCAN",
+            NextValue(self.uart_master_status, 0),  # UART is no longer master
+            NextValue(self.uart_slave_status, 1),
+            NextValue(self.bus.stb, 0),
+            NextValue(self.bus.cyc, 0),
+            NextValue(self.bus.we, 0),
+            NextValue(self.debug_read_enable, 0),
+            If(self.time_counter >= (20),  # about ~10s
                 NextValue(self.time_counter, 0),
                 NextValue(self.addr, 0x20000000),  # Reset address
                 NextState("BECOME_MASTER")  # Restart reading
@@ -244,19 +325,22 @@ class DualMasterSoC(SoCCore):
         # 2 masters: AES and UART
         self.aes_master = wishbone.Interface()
         self.uart_master = wishbone.Interface()
+        self.random_master = wishbone.Interface()
 
         self.submodules.aes = AESFromRAM(self.aes_master)
         self.submodules.uart_spy = UARTSpy(self.uart_master)
+        self.submodules.random_component = UARTSpy(self.random_master)
         self.submodules.bus_monitor = GenericBusMonitor(self.ram.bus)
 
         # Wishbone arbiter
-        self.submodules.arbiter = Arbiter([self.aes_master, self.uart_master], self.ram.bus)
+        self.submodules.arbiter = Arbiter([self.aes_master, self.uart_master, self.random_master], self.ram.bus)
 
         # Memory map
         self.bus.add_slave("shared_ram", self.ram.bus,region=SoCRegion(origin=0x20000000, size=0x1000))
         self.bus.add_master("aes", master=self.aes_master)
         self.bus.add_slave("uart", self.uart_master, region=SoCRegion(origin=0x30000000, size=0x1000))  # Example address region for UART
-        
+        self.bus.add_slave("random", self.random_master, region=SoCRegion(origin=0x30010000, size=0x1000))  # Example address region for UART
+
 # ----------- Simulation Testbench -----------
 def tb(dut):
     start_time = time()
@@ -301,7 +385,7 @@ def tb(dut):
     last_uart_master_status = uart_master_status
 
     while True:
-        if time() - start_time > 25:
+        if time() - start_time > 3:
             print("Simulation finished.")
             break
         uart_master_status = yield dut.uart_spy.uart_master_status
